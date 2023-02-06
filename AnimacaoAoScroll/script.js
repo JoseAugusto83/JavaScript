722fd2b173ae9589ca80b8d5bc3e9050ccf52e58
José Augusto <ja834136@gmail.com>
2023-02-06 14:47:39 -0300

    Animação ao scroll

---
+++ AnimacaoAoScroll/script.js
@@ -0,0 +1,16 @@
+let sections = document.querySelectorAll('section')
+
+console.log(sections.length)
+
+function animacaoScroll(){
+    for(let i = 0; i < sections.length; i += 1){
+        let distanciaTop = sections[i].getBoundingClientRect().top;
+        let section = sections[i];
+        if(distanciaTop <= 500){
+            section.classList.add('ativo');
+        }
+    }
+}
+
+
+window.addEventListener('scroll', animacaoScroll)
